8dc818a08f1449f9a4e494f824f845d54c2b1c10
drake-vcu <dkaempfer46@gmail.com>
2019-03-26 19:16:51 -0500

    Add project files.

---
+++ BeyondCloudPingConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BeyondCloudPingConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
